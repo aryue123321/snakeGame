99cd4ee51f8eecdd4a0e73ca5f5688b1fad85e3b
Chun Yue Leung <aryue123321@gmail.com>
2021-10-03 21:24:15 -0700

    remove useless stuff

---
+++ src/BoardGen/BoardGenServie.ts
@@ -0,0 +1,161 @@
+
+export enum Direction {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+export enum BoardCellType{
+  Empty = 0,
+  Snake,
+  Food
+}
+
+type Cell = {
+  x: number,
+  y: number
+}
+
+
+
+export type GameBoard = {
+  cells: BoardCellType[][],
+  snake: Cell[],
+  food: Cell|null,
+  direction: Direction,
+  isValid: boolean,
+  score: number
+}
+
+function getEmptyCells(cells: BoardCellType[][]): number[][]{
+  const res = []
+  for(var i = 0; i < cells.length; i++){
+    for(var j = 0; j < cells[0].length; j++){
+      if(cells[i][j] === BoardCellType.Empty){
+        res.push([i, j])
+      }
+    }
+  }
+  return res;
+}
+function genNextFoodCell(cells: BoardCellType[][]): Cell|null{
+  const emptyCells = getEmptyCells(cells)
+  if(!emptyCells.length){
+    return null
+  }
+  const nextFoodCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+  return {
+    x: nextFoodCell[0],
+    y: nextFoodCell[1]
+  }
+}
+
+function getNextCell(gameBoard: GameBoard, direction:Direction|null = null): Cell{
+  const head = gameBoard.snake[0]
+  const boardSize= gameBoard.cells.length;
+  direction = direction ? direction : gameBoard.direction;
+  switch (direction){
+    case Direction.Right:
+      return {
+        x: head.x, 
+        y: (head.y + 1) % boardSize
+      }
+    case Direction.Left:
+      return {
+        x: head.x, 
+        y:(head.y - 1 + boardSize) % boardSize
+      }
+    case Direction.Down:
+      return {
+        x: (head.x + 1) % boardSize, 
+        y: head.y
+      }
+    case Direction.Up:
+      return {
+        x: (head.x - 1 + boardSize) % boardSize, 
+        y: head.y
+      }
+  }
+}
+
+export const getNextBoard = (gameBoard: GameBoard) : GameBoard =>{
+
+  const nextHead = getNextCell(gameBoard);
+  const tail = gameBoard.snake[gameBoard.snake.length-1]
+  const newBoard: GameBoard = JSON.parse(JSON.stringify(gameBoard))
+
+  switch(gameBoard.cells[nextHead.x][nextHead.y]){
+    case BoardCellType.Empty:
+      newBoard.snake.unshift(nextHead)
+      newBoard.snake.pop();
+      newBoard.cells[nextHead.x][nextHead.y] = BoardCellType.Snake
+      newBoard.cells[tail.x][tail.y] = BoardCellType.Empty
+      break;
+    case BoardCellType.Food:
+      newBoard.snake.unshift(nextHead)
+      newBoard.cells[nextHead.x][nextHead.y] = BoardCellType.Snake
+      const nextFoodCell = genNextFoodCell(newBoard.cells)
+      if(nextFoodCell){
+        newBoard.cells[nextFoodCell.x][nextFoodCell.y] = BoardCellType.Food
+        newBoard.score++;
+      }else{
+        newBoard.isValid = false;
+      }
+      break;
+    case BoardCellType.Snake:
+      if(nextHead.x === tail.x && nextHead.y === tail.y){
+        newBoard.snake.unshift(nextHead)
+        newBoard.snake.pop();
+      }
+      else{
+        newBoard.isValid = false
+      }
+      break;
+  }
+
+  return newBoard;
+}
+
+export const InitiBoard = (boardSize: number): GameBoard => {
+  const cells = new Array(boardSize).fill(0).map(a=>new Array(boardSize).fill(BoardCellType.Empty));
+  const snakeLength = Math.ceil(boardSize/2);
+  const snakeCells: number[][] = [];
+  for(var i = Math.ceil(snakeLength/2); i < snakeLength + Math.floor(snakeLength/2); i++){
+    snakeCells.push([snakeLength,i])
+  }
+  const snake = snakeCells.map(s=>{return {x: s[0], y: s[1]}})
+  for(const s of snakeCells){
+    cells[s[0]][s[1]] = BoardCellType.Snake
+  }
+  const food = genNextFoodCell(cells);
+  if(food){
+    cells[food.x][food.y] = BoardCellType.Food;
+  }
+  return {
+    cells,
+    snake,
+    food,
+    direction: Direction.Left,
+    isValid: true,
+    score : 0
+  }
+}
+
+export const updateDirection = (gameBoard: GameBoard, direction: Direction): GameBoard => {
+  const newBoard: GameBoard = JSON.parse(JSON.stringify(gameBoard))
+  if( 
+    ((gameBoard.direction === Direction.Up || gameBoard.direction === Direction.Down) && (direction === Direction.Left || direction === Direction.Right)) ||
+    ((gameBoard.direction === Direction.Left || gameBoard.direction === Direction.Right) && (direction === Direction.Up || direction === Direction.Down))
+  )
+  {
+    const nextCell = getNextCell(newBoard, direction)
+    const snakeSecondCell = newBoard.snake[1];
+    console.log(nextCell, snakeSecondCell)
+    if(nextCell.x !== snakeSecondCell.x || nextCell.y !== snakeSecondCell.y){
+      newBoard.direction = direction;
+      // return getNextBoard(newBoard);
+    }
+  }
+  return newBoard
+}
---
+++ src/components/Board.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+
+import * as bg from '../BoardGen/BoardGenServie'
+import './Board.scss'
+import { useEffect, useState, useRef } from 'react';
+
+const Board = () =>{
+  const initBoardSize = 20
+  const [board, setBoard] = useState(()=> bg.InitiBoard(initBoardSize))
+  const [myInterval, setMyInterval] = useState<any>(null)
+  const boardEl = useRef<HTMLDivElement>(null)
+  const [running, setRunning] = useState(true)
+
+  useEffect(()=>{
+    if(running){
+      console.log('load')
+      const interval = setInterval(()=>{
+        setBoard(board => bg.getNextBoard(board))
+      }, 50)
+      setMyInterval(interval);
+      return (()=>clearInterval(interval))
+    }
+  }, [running])
+
+ useEffect(()=>{
+    if(!board.isValid){
+      clearInterval(myInterval);
+      setRunning(false);
+      alert(`score : ${board.score}`)
+    }
+ }, [board, myInterval])
+
+ useEffect(() => {
+  function activeCellListenser(e:any){
+    console.log(e.code)
+    if(e.code === 'ArrowDown' || e.code === 'KeyS'){
+      update(bg.Direction.Down)
+    }
+    if(e.code === 'ArrowUp' || e.code === 'KeyW'){
+      update(bg.Direction.Up)
+    }
+    if(e.code === 'ArrowRight' || e.code === 'KeyD'){
+      update(bg.Direction.Right)
+    }
+    if(e.code === 'ArrowLeft' || e.code === 'KeyA'){
+      update(bg.Direction.Left)
+    }
+    
+  }
+
+  document.addEventListener('keydown', activeCellListenser);
+  return (()=> document.removeEventListener('keydown', activeCellListenser));
+
+}, [running])
+
+  function update(direction: bg.Direction){
+    if(running){
+      console.log('update', running)
+      setBoard(board => bg.updateDirection(board,direction ));
+    }
+  }
+ 
+  function getCellClass(cellType: bg.BoardCellType):string{
+    switch(cellType){
+      case bg.BoardCellType.Empty:
+        return 'empty';
+      case bg.BoardCellType.Food:
+        return 'food';
+      case bg.BoardCellType.Snake:
+        return 'snake';
+    }
+  }
+  function getSnakeGradient(cellType: bg.BoardCellType, x:number, y:number):number{
+    if(cellType !== bg.BoardCellType.Snake)
+      return 1;
+    const index = board.snake.findIndex(c=>c.x === x && c.y === y)
+    return Math.max( (board.snake.length - index + 3)/(board.snake.length+3), 0.5)
+  }
+
+  function reset(){
+    setBoard(bg.InitiBoard(initBoardSize))
+    setRunning(true);
+
+  }
+
+  return <div className="game">
+    <div>Score : {board.score}</div>
+
+      <div className="game-board" ref={boardEl}>
+      {board.cells.map((row, rowIndex)=>
+      <div key={rowIndex} className="game-board-row">
+        {row.map((col, colIndex) => <div key={colIndex} className={`game-board-cell ${getCellClass(col)}`} style={{opacity:getSnakeGradient(col, rowIndex, colIndex)}}></div>)}
+      </div>
+      )}
+    </div>
+
+    
+    {!running && <button className="ui button" onClick={()=> reset()} style={{marginTop:'10px'}}>Replay</button>}
+  </div>
+}
+
+export default Board;
